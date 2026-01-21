6dd237ec2aa965dc1da5ce8399d9294740cfa099
ashish gautam <24155760@kiit.ac.in>
2026-01-21 19:39:41 +0530

    weekday

---
+++ grade.java
@@ -0,0 +1,17 @@
+class Grade {
+    public static void main(String[] args) {
+        int marks = 85;   // Mention the marks here
+
+        if (marks >= 90) {
+            System.out.println("Grade: O");
+        } else if (marks >= 80) {
+            System.out.println("Grade: E");
+        } else if (marks >= 70) {
+            System.out.println("Grade: A");
+        } else if (marks >= 60) {
+            System.out.println("Grade: B");
+        } else {
+            System.out.println("Grade: C");
+        }
+    }
+}
